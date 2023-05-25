24d1b43ba768b9099b23d6fb79d8a2d744eb7118
Kota <k019g1380.s.kotaro@gmail.com>
2023-05-25 12:37:05 +0900

    授業内で調整

---
+++ Assets/layingCard.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class layingCard : MonoBehaviour
+{
+    public GameObject _card01, _card02;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
