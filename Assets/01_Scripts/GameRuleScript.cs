53d66d00f86675b8fc4bbb357ec0a48094d33194
Kota <k019g1380.s.kotaro@gmail.com>
2023-07-19 14:24:38 +0900

    HowTo置換

--- Assets/01_Scripts/GameRuleScript.cs
+++ Assets/01_Scripts/GameRuleScript.cs
@@ -6,6 +6,12 @@ public class GameRuleScript : MonoBehaviour
 {
     public GameObject _player;
     public GameObject _startButton;
+    public GameObject _nextButton;
+    public GameObject _prevButton;
+    public GameObject _titleButton;
+
+    public GameObject _howTo01;
+    public GameObject _howTo02;
 
     void Update()
     {
@@ -17,7 +23,26 @@ public class GameRuleScript : MonoBehaviour
         {
             _player.SetActive(true);
         }
-
-        if (this.gameObject.activeSelf) _startButton.SetActive(true);
     }
+
+    public void Next()
+    {
+        _howTo02.SetActive(true);
+        _howTo01.SetActive(false);
+        _startButton.SetActive(true);
+        _prevButton.SetActive(true);
+        _nextButton.SetActive(false);
+        _titleButton.SetActive(false);
+    }
+
+    public void Prev()
+    {
+        _howTo02.SetActive(false);
+        _howTo01.SetActive(true);
+        _startButton.SetActive(false);
+        _prevButton.SetActive(false);
+        _nextButton.SetActive(true);
+        _titleButton.SetActive(true);
+    }
+
 }
--- Assets/01_Scripts/HowToPlayScript.cs
+++ Assets/01_Scripts/HowToPlayScript.cs
@@ -4,31 +4,55 @@ using UnityEngine;
 
 public class HowToPlayScript : MonoBehaviour
 {
-    public GameObject _howToPlayPanel;
+    public GameObject _howToPlayPanel01;
+    public GameObject _howToPlayPanel02;
     public GameObject _backButton;
-    
+    public GameObject _nextButton;
+    public GameObject _prevButton;
+    public GameObject _startButton;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void HowToPlay()
     {
-        _howToPlayPanel.SetActive(true);
+        _howToPlayPanel01.SetActive(true);
         _backButton.SetActive(true);
+        _nextButton.SetActive(true);
 
+        _howToPlayPanel02.SetActive(false);
+        _prevButton.SetActive(false);
+        _startButton.SetActive(false);
+    }
+
+    public void Next()
+    {
+        _howToPlayPanel01.SetActive(false);
+        _backButton.SetActive(false);
+        _nextButton.SetActive(false);
+
+        _howToPlayPanel02.SetActive(true);
+        _prevButton.SetActive(true);
+        _startButton.SetActive(true);
     }
     public void Back()
     {
-        _howToPlayPanel.SetActive(false);
+        _howToPlayPanel01.SetActive(false);
         _backButton.SetActive(false);
+        _nextButton.SetActive(false);
+
+        _howToPlayPanel02.SetActive(false);
+        _prevButton.SetActive(false);
+        _startButton.SetActive(false);
     }
 }
